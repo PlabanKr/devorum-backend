d99b864575098f8b728cc3444eba99f4ced366eb
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2024-08-09 10:57:43 +0530

    setup initial user route

---
+++ routes/v1/users/api.user.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+
+// database connection pool
+const { pool } = require("../../../database/postgres.database.js");
+
+// get user by id
+router.get("/:id", (req, res) => {
+  try {
+    const id = req.params.id;
+    pool.query(
+      "SELECT * FROM users WHERE user_id = $1",
+      [id],
+      (error, results) => {
+        if (error) {
+          throw error;
+        }
+        res.status(200).json(results.rows);
+      }
+    );
+  } catch (error) {
+    console.log("Error: ", error);
+    res.status(500).send("Internal Server Error\n" + error);
+  }
+});
+
+export default router;
