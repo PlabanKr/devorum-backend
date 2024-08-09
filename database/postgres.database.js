456b33436b5172921629546c126a9470447a5f12
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2024-08-09 10:43:16 +0530

    add database connection pool

---
+++ database/postgres.database.js
@@ -0,0 +1,12 @@
+const pg = require("pg");
+const Pool = pg.Pool;
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT),
+  password: process.env.DB_PASSWORD,
+});
+
+export default pool;
