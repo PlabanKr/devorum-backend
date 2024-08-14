c556b119bcf480c2fd83e4d6f09fc708c01d71c7
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2024-08-14 12:44:20 +0530

    Add forum routes

--- routes/v1/api.v1.js
+++ routes/v1/api.v1.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const userRoutes = require("./users/api.user.js");
+const forumRoutes = require("./forums/api.forums.js");
 
 router.use("/user", userRoutes);
+router.use("/forum", forumRoutes);
 
 module.exports = router;
