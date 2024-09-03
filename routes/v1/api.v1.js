e1cdf167e7a7dfe31e9239057d2cde0ca0992862
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2024-09-03 15:29:29 +0530

    Connect Idea APIs

--- routes/v1/api.v1.js
+++ routes/v1/api.v1.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const userRoutes = require("./users/api.user.js");
 const forumRoutes = require("./forums/api.forums.js");
+const ideaRoutes = require("./ideas/api.idea.js");
 
 router.use("/user", userRoutes);
 router.use("/forum", forumRoutes);
+router.use("/idea", ideaRoutes);
 
 module.exports = router;
