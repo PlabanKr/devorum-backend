12d6763bf955d8dd2a41c8f614318cb47e3dafa3
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2024-09-22 18:47:30 +0530

    Add forums joined api

--- routes/v1/api.v1.js
+++ routes/v1/api.v1.js
@@ -5,10 +5,12 @@ const userRoutes = require("./users/api.user.js");
 const forumRoutes = require("./forums/api.forums.js");
 const ideaRoutes = require("./ideas/api.idea.js");
 const ideaInterestedRoutes = require("./interests/api.interest.js");
+const forumJoinedRoutes = require("./forums_joined/api.forums.joined.js");
 
 router.use("/user", userRoutes);
 router.use("/forum", forumRoutes);
 router.use("/idea", ideaRoutes);
 router.use("/interest", ideaInterestedRoutes);
+router.use("/forum-joined", forumJoinedRoutes);
 
 module.exports = router;
