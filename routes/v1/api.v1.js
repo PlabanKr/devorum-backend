308146cb9856e78ea3c1dfb61fb64b64f0d10065
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2024-10-31 21:03:50 +0530

    Add connections route

--- routes/v1/api.v1.js
+++ routes/v1/api.v1.js
@@ -6,11 +6,13 @@ const forumRoutes = require("./forums/api.forums.js");
 const ideaRoutes = require("./ideas/api.idea.js");
 const ideaInterestedRoutes = require("./interests/api.interest.js");
 const forumJoinedRoutes = require("./forums_joined/api.forums.joined.js");
+const connectionsRoutes = require("./connections/api.connections.js");
 
 router.use("/user", userRoutes);
 router.use("/forum", forumRoutes);
 router.use("/idea", ideaRoutes);
 router.use("/interest", ideaInterestedRoutes);
 router.use("/forum-joined", forumJoinedRoutes);
+router.use("/connection", connectionsRoutes);
 
 module.exports = router;
