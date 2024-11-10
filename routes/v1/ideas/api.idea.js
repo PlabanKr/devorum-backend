7499bc7d2ef56e9d5b8144673f703c6a69b7eecf
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2024-11-10 18:33:00 +0530

    Add route for getting all recent ideas by user id

--- routes/v1/ideas/api.idea.js
+++ routes/v1/ideas/api.idea.js
@@ -106,6 +106,31 @@ router.get("/user/:id", (req, res) => {
   }
 });
 
+// get recent ideas by user_id (example: localhost:5000/api/v1/idea/user/1/recent OR localhost:5000/api/v1/idea/user/1/recent?limit=1000&page=1)
+router.get("/user/:id/recent", (req, res) => {
+  try {
+    const limit = parseInt(req.query.limit, 20) || 20; // Default to 20 if not provided or invalid
+    const page = parseInt(req.query.page, 10) || 1; // Default to 1 if not provided or invalid
+
+    // Calculate offset, ensure it's non-negative
+    const offset = Math.max((page - 1) * limit, 0);
+    const id = req.params.id;
+    pool.query(
+      "SELECT * FROM ideas WHERE user_id = $1 ORDER BY created_at DESC LIMIT $2 OFFSET $3",
+      [id, limit, offset],
+      (error, results) => {
+        if (error) {
+          throw error;
+        }
+        return res.status(200).json(results.rows);
+      }
+    );
+  } catch (error) {
+    console.log("Error: ", error);
+    return res.status(500).send("Internal Server Error\n" + error);
+  }
+});
+
 // get idea by forum_id (example: localhost:5000/api/v1/idea/forum/1 OR localhost:5000/api/v1/idea/forum/1?limit=1000&page=1)
 router.get("/forum/:id", (req, res) => {
   try {
